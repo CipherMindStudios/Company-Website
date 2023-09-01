3d946e9b33171eb196e40512353f1b89e1fed34d
CipherMindStudios <143823500+CipherMindStudios@users.noreply.github.com>
2023-09-02 01:02:47 +0500

    Minor Changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const toRotate = [ "Flutter", "Flutter Flow", "Ruby on Rails","MERN Stack"  ];
   const period = 2000;
 
   useEffect(() => {
@@ -55,9 +55,9 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+
+                <h1>{`Hi! We Are CipherMind We Are Expert in `} <span className="txt-rotate" dataPeriod="1000" data-rotate='["Flutter", "Flutter Flow", "Ruby on Rails","MERN Stack" ]'><span className="wrap">{text}</span></span></h1>
+                  <p>Our team at <strong>CipherMinds Studio</strong> is dedicated to delivering high-quality, scalable, and user-friendly solutions using these technologies. If you have any specific projects or questions related to these skills, please feel free to ask, and we'll be happy to assist you further.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
